f8cdb9411788d4cee496bdb531e23e6d4476a388
HanzaRu <john.rosa@aquiris.com.br>
2020-09-14 12:18:21 -0300

    Editor definitions

--- Editor/PackageUpdater.cs
+++ Editor/PackageUpdater.cs
@@ -1,4 +1,5 @@
-﻿using UnityEditor;
+﻿#if UNITY_EDITOR
+using UnityEditor;
 using UnityEditor.PackageManager;
 
 namespace GameWorkstore.Patterns
@@ -12,3 +13,4 @@ namespace GameWorkstore.Patterns
         }
     }
 }
+#endif
