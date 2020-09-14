b4b32813648404f1c7f5a6d651d3956b9d4d0c85
HanzaRu <john.rosa@aquiris.com.br>
2020-09-13 22:23:33 -0300

    Package updater

---
+++ Editor/PackageUpdater.cs
@@ -0,0 +1,11 @@
+﻿using UnityEditor;
+using UnityEditor.PackageManager;
+
+public class PackageUpdater
+{
+    [MenuItem("Help/PackageUpdate/Patterns")]
+    public static void TrackPackages()
+    {
+        Client.Add("git://github.com/GameWorkstore/patterns.git");
+    }
+}
