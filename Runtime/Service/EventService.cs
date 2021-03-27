0c803d37ee322b98a94c0730d76030cdce73fba2
Hanza Ru <john.rosa@playkids.com>
2021-03-27 11:04:09 -0300

    +version 1.0.8
    Added support for localize concrete services from abstract definition (cache it if possible for avoid cpu usage).

---
+++ Runtime/Service/EventService.cs
@@ -0,0 +1,119 @@
+﻿using System.Collections;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GameWorkstore.Patterns
+{
+    public class EventService : IService
+    {
+        private EventServiceMonobehaviour _behaviour;
+
+        public Signal Update;
+        public Signal LateUpdate;
+        public Signal ApplicationQuit;
+        public Signal<bool> ApplicationFocus;
+        public Queue<Action> ActionsPerFrame = new Queue<Action>();
+
+        public override void Preprocess()
+        {
+            _behaviour = new GameObject("EventService").AddComponent<EventServiceMonobehaviour>();
+            _behaviour.EventService = this;
+            if (!Application.isPlaying) return;
+            UnityEngine.Object.DontDestroyOnLoad(_behaviour.gameObject);
+        }
+
+        public override void Postprocess()
+        {
+            UnityEngine.Object.Destroy(_behaviour.gameObject);
+        }
+
+        public Coroutine StartCoroutine(IEnumerator coroutine)
+        {
+            return _behaviour.StartCoroutine(coroutine);
+        }
+
+        public Coroutine QueueDelayedAction(Action action, float time)
+        {
+            return _behaviour.StartCoroutine(DelayedAction(action, time));
+        }
+
+        private IEnumerator DelayedAction(Action action, float time)
+        {
+            yield return new WaitForSecondsRealtime(time);
+            action?.Invoke();
+        }
+
+        public void QueueAction(Action action)
+        {
+            ActionsPerFrame.Enqueue(action);
+        }
+
+        public void StopCoroutine(Coroutine coroutine)
+        {
+            _behaviour.StopCoroutine(coroutine);
+        }
+
+        internal void ExecuteUpdate()
+        {
+            Update.Invoke();
+            if (ActionsPerFrame.Count > 0)
+            {
+                Action action = ActionsPerFrame.Dequeue();
+                try
+                {
+                    action();
+                }
+                catch(Exception e)
+                {
+                    Debug.LogError(e.Message + ":\n" + e.StackTrace);
+                }
+            }
+        }
+
+        internal void ExecuteLateUpdate()
+        {
+            LateUpdate.Invoke();
+        }
+
+        internal void ExecuteApplicationQuit()
+        {
+            ApplicationQuit.Invoke();
+        }
+
+        internal void ExecuteApplicationFocus(bool focus)
+        {
+            ApplicationFocus.Invoke(focus);
+        }
+    }
+
+    public class EventServiceMonobehaviour : MonoBehaviour
+    {
+        internal EventService EventService;
+
+        public void Update()
+        {
+            EventService.ExecuteUpdate();
+        }
+
+        public void LateUpdate()
+        {
+            EventService.ExecuteLateUpdate();
+        }
+
+        public void OnApplicationFocus(bool focus)
+        {
+            EventService.ExecuteApplicationFocus(focus);
+        }
+
+        public void OnApplicationQuit()
+        {
+            EventService.ExecuteApplicationQuit();
+        }
+
+        public void OnDestroy()
+        {
+            ServiceProvider.Shutdown();
+        }
+    }
+}
---
+++ Runtime/Service/ServiceProvider.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace GameWorkstore.Patterns
+{
+    public static class ServiceProvider
+    {
+        private static readonly Dictionary<Type, IService> _services = new Dictionary<Type, IService>();
+        private static readonly ServicePriorityComparer _serviceSorter = new ServicePriorityComparer();
+
+        public static T GetService<T>() where T : IService
+        {
+            Type type = typeof(T);
+            if (_services.TryGetValue(type, out IService service))
+            {
+                return service as T;
+            }
+            if (type.IsAbstract)
+            {
+                foreach(var pair in _services)
+                {
+                    if(pair.Value is T)
+                    {
+                        return pair.Value as T;
+                    }
+                }
+                Debug.LogError("[ServiceProvider]: No concrete service initialized for this abstraction.");
+                return null;
+            }
+            service = (IService)Activator.CreateInstance(typeof(T));
+            if(service == null)
+            {
+                Debug.LogError("[ServiceProvider]: Error while creating " + type.Name + " service.");
+                return null;
+            }
+            service.Preprocess();
+            _services[type] = service;
+            return service as T;
+        }
+
+        public static bool RemoveService<T>() where T : IService
+        {
+            Type type = typeof(T);
+            if (!_services.ContainsKey(type)) return false;
+            IService service = _services[type];
+            service.Postprocess();
+            _services.Remove(type);
+            return true;
+        }
+
+        /// <summary>
+        /// Destroy and clear all services.
+        /// </summary>
+        public static void Shutdown()
+        {
+            List<IService> services = _services.Values.ToList();
+            services.Sort(_serviceSorter);
+            foreach (IService service in services)
+            {
+                service.Postprocess();
+            }
+            _services.Clear();
+        }
+
+        public class ServicePriorityComparer : IComparer<IService>
+        {
+            public int Compare(IService x, IService y)
+            {
+                return x.Priority().CompareTo(y.Priority());
+            }
+        }
+    }
+}
