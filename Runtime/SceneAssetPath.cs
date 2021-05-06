acec284154b11e5fa7193b0adfb4c52f682b49f0
Hanza Ru <john.rosa@playkids.com>
2021-05-06 17:39:42 -0300

    +version 1.1.2
    +Added Trim() function to HighSpeedArray
    +Fixed SetCapacity() function to change capacity always
    +Ensure consistency in SetCapacity() function and final Count of elements

---
+++ Runtime/SceneAssetPath.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace GameWorkstore.Patterns
+{
+    [Serializable]
+    public class SceneAssetPath
+    {
+        public string Scene;
+        public string Guid;
+
+        public void OnValidate()
+        {
+#if UNITY_EDITOR
+            var oldScenePath = UnityEditor.AssetDatabase.LoadAssetAtPath<UnityEditor.SceneAsset>(Scene);
+            if (oldScenePath) return;
+
+            var path = UnityEditor.AssetDatabase.GUIDToAssetPath(Guid);
+            if (string.IsNullOrEmpty(path)) return;
+            Scene = path;
+#endif
+        }
+    }
+}
