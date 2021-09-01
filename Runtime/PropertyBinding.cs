45231f178f8ea0d086020a33e767414bf5d66ef6
Hanza Ru <john.rosa@playkids.com>
2021-09-01 03:24:23 -0300

    +Signal now can be used with NSubstitute

---
+++ Runtime/PropertyBinding.cs
@@ -0,0 +1,37 @@
+﻿namespace GameWorkstore.Patterns
+{
+
+    public abstract class PropertyBinding<T>
+    {
+        private bool _setup = false;
+        public T Value = default;
+        public Signal<T> OnChange;
+
+        public void Test(T valueT)
+        {
+            if (valueT.Equals(Value) && _setup) return;
+
+            _setup = true;
+            Value = valueT;
+            OnChange.Invoke(valueT);
+        }
+    }
+
+    public abstract class PropertyBinding<T, U>
+    {
+        private bool _setup = false;
+        public T ValueT = default(T);
+        public U ValueU = default(U);
+        public Signal<T, U> OnChange;
+
+        public void Test(T valueT, U valueU)
+        {
+            if (ValueT.Equals(valueT) && ValueU.Equals(valueU) && _setup) return;
+
+            _setup = true;
+            ValueT = valueT;
+            ValueU = valueU;
+            OnChange.Invoke(valueT, valueU);
+        }
+    }
+}
