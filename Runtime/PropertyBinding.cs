f2f769cfd588b180de965d0799d577fb21a9c4a3
Hanza Ru <john.rosa@playkids.com>
2021-09-02 22:15:11 -0300

    +PropertyBinding initializes registering events with current value.

---
+++ Runtime/PropertyBinding.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace GameWorkstore.Patterns
+{
+    public abstract class PropertyBinding<T>
+    {
+        private bool _setup = false;
+        public T Value = default;
+        private readonly Signal<T> _onChange;
+
+        public void Set(T valueT)
+        {
+            if (valueT.Equals(Value) && _setup) return;
+
+            _setup = true;
+            Value = valueT;
+            _onChange.Invoke(valueT);
+        }
+
+        public void Register(Action<T> e)
+        {
+            _onChange.Register(e);
+            e?.Invoke(Value);
+        }
+
+        public void Unregister(Action<T> e)
+        {
+            _onChange.Unregister(e);
+        }
+
+        public void Empty()
+        {
+            _onChange.Empty();
+        }
+    }
+}
---
+++ Runtime/Signal/Signal.cs
@@ -0,0 +1,104 @@
+﻿using System;
+
+namespace GameWorkstore.Patterns
+{
+    public struct Signal : ISignal
+    {
+        public event Action Action;
+
+        public void Empty()
+        {
+            Action = null;
+        }
+
+        public void Invoke()
+        {
+            Action?.Invoke();
+        }
+
+        public void Register(Action e)
+        {
+            Action += e;
+        }
+
+        public void Unregister(Action e)
+        {
+            Action -= e;
+        }
+    }
+
+    public struct Signal<T> : ISignal<T>
+    {
+        public event Action<T> Action;
+
+        public void Empty()
+        {
+            Action = null;
+        }
+
+        public void Invoke(T evt)
+        {
+            Action?.Invoke(evt);
+        }
+
+        public void Register(Action<T> e)
+        {
+            Action += e;
+        }
+
+        public void Unregister(Action<T> e)
+        {
+            Action -= e;
+        }
+    }
+
+    public struct Signal<T, U> : ISignal<T, U>
+    {
+        public event Action<T, U> Action;
+
+        public void Empty()
+        {
+            Action = null;
+        }
+
+        public void Invoke(T evt, U evu)
+        {
+            Action?.Invoke(evt, evu);
+        }
+
+        public void Register(Action<T, U> e)
+        {
+            Action += e;
+        }
+
+        public void Unregister(Action<T, U> e)
+        {
+            Action -= e;
+        }
+    }
+
+    public struct Signal<T, U, V> : ISignal<T, U, V>
+    {
+        public event Action<T, U, V> Action;
+
+        public void Empty()
+        {
+            Action = null;
+        }
+
+        public void Invoke(T evt, U evu, V evv)
+        {
+            Action?.Invoke(evt, evu, evv);
+        }
+
+        public void Register(Action<T, U, V> e)
+        {
+            Action += e;
+        }
+
+        public void Unregister(Action<T, U, V> e)
+        {
+            Action -= e;
+        }
+    }
+}
