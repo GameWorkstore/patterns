4f9913b0b110322be6d17a57a20ae434e8dd834c
John Willy de Souza Rosa <woodimbr@gmail.com>
2022-07-21 17:24:09 +0200

    +Await.AsyncOp(op) now accept null without break.

---
+++ Runtime/Async/UnityAsyncManager.cs
@@ -0,0 +1,191 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace GameWorkstore.Patterns
+{
+    // manages all AsyncBehaviours including low overhead update calls
+    public class UnityAsyncManager : MonoBehaviour
+    {
+        public static int FrameCount;
+        public static uint FixedStepCount;
+        public static float Time;
+        public static float UnscaledTime;
+
+        public static AsyncBehaviour Behaviour { get; private set; }
+
+        private static List<IAsyncBehaviour> _updates;
+        private static List<IAsyncBehaviour> _lateUpdates;
+        private static List<IAsyncBehaviour> _fixedUpdates;
+
+        [RuntimeInitializeOnLoadMethod]
+        static void Initialize()
+        {
+            var anchor = new GameObject("UnityAsync Manager").AddComponent<UnityAsyncManager>();
+            anchor.gameObject.hideFlags = HideFlags.HideAndDontSave;
+            DontDestroyOnLoad(anchor.gameObject);
+
+            FrameCount = 1;
+            FixedStepCount = 1;
+            Time = UnityEngine.Time.time;
+            UnscaledTime = UnityEngine.Time.unscaledTime;
+
+            _updates = new List<IAsyncBehaviour>(128);
+            _lateUpdates = new List<IAsyncBehaviour>(32);
+            _fixedUpdates = new List<IAsyncBehaviour>(32);
+
+            Behaviour = anchor.gameObject.AddComponent<AsyncBehaviour>();
+        }
+
+        public static void RegisterUpdate(IAsyncBehaviour b)
+        {
+            b.UpdateIndex = _updates.Count;
+            _updates.Add(b);
+        }
+
+        public static void RegisterLateUpdate(IAsyncBehaviour b)
+        {
+            b.LateUpdateIndex = _lateUpdates.Count;
+            _lateUpdates.Add(b);
+        }
+
+        public static void RegisterFixedUpdate(IAsyncBehaviour b)
+        {
+            b.FixedUpdateIndex = _fixedUpdates.Count;
+            _fixedUpdates.Add(b);
+        }
+
+        public static void UnregisterUpdate(IAsyncBehaviour b)
+        {
+            int count = _updates.Count;
+            int i = b.UpdateIndex;
+
+            if (count > 1)
+            {
+                var toSwap = _updates[count - 1];
+                _updates[i] = toSwap;
+                toSwap.UpdateIndex = i;
+                _updates.RemoveAt(count - 1);
+            }
+            else
+            {
+                _updates.RemoveAt(i);
+            }
+        }
+
+        public static void UnregisterLateUpdate(IAsyncBehaviour b)
+        {
+            int count = _fixedUpdates.Count;
+            int i = b.LateUpdateIndex;
+
+            if (count > 1)
+            {
+                var toSwap = _lateUpdates[count - 1];
+                _lateUpdates[i] = toSwap;
+                toSwap.LateUpdateIndex = i;
+                _lateUpdates.RemoveAt(count - 1);
+            }
+            else
+            {
+                _lateUpdates.RemoveAt(i);
+            }
+        }
+
+        public static void UnregisterFixedUpdate(IAsyncBehaviour b)
+        {
+            int count = _fixedUpdates.Count;
+            int i = b.FixedUpdateIndex;
+
+            if (count > 1)
+            {
+                var toSwap = _fixedUpdates[count - 1];
+                _fixedUpdates[i] = toSwap;
+                toSwap.FixedUpdateIndex = i;
+                _fixedUpdates.RemoveAt(count - 1);
+            }
+            else
+            {
+                _fixedUpdates.RemoveAt(i);
+            }
+        }
+
+        void Update()
+        {
+            // = Time.frameCount;
+            Time = UnityEngine.Time.time;
+            UnscaledTime = UnityEngine.Time.unscaledTime;
+
+            for (int i = 0; i < _updates.Count; ++i)
+            {
+                if (!_updates[i].Update())
+                {
+                    int count = _updates.Count;
+
+                    if (count > 1)
+                    {
+                        var toSwap = _updates[count - 1];
+                        _updates[i] = toSwap;
+                        toSwap.UpdateIndex = i;
+                        _updates.RemoveAt(count - 1);
+                        --i;
+                    }
+                    else
+                    {
+                        _updates.RemoveAt(i);
+                    }
+                }
+            }
+
+            ++FrameCount;
+        }
+
+        void LateUpdate()
+        {
+            for (int i = 0; i < _lateUpdates.Count; ++i)
+            {
+                if (!_lateUpdates[i].LateUpdate())
+                {
+                    int count = _lateUpdates.Count;
+
+                    if (count > 1)
+                    {
+                        var toSwap = _lateUpdates[count - 1];
+                        _lateUpdates[i] = toSwap;
+                        toSwap.LateUpdateIndex = i;
+                        _lateUpdates.RemoveAt(count - 1);
+                        --i;
+                    }
+                    else
+                    {
+                        _lateUpdates.RemoveAt(i);
+                    }
+                }
+            }
+        }
+
+        void FixedUpdate()
+        {
+            for (int i = 0; i < _fixedUpdates.Count; ++i)
+            {
+                if (!_fixedUpdates[i].FixedUpdate())
+                {
+                    int count = _fixedUpdates.Count;
+
+                    if (count > 1)
+                    {
+                        var toSwap = _fixedUpdates[count - 1];
+                        _fixedUpdates[i] = toSwap;
+                        toSwap.FixedUpdateIndex = i;
+                        _fixedUpdates.RemoveAt(count - 1);
+                        --i;
+                    }
+                    else
+                    {
+                        _fixedUpdates.RemoveAt(i);
+                    }
+                }
+            }
+
+            ++FixedStepCount;
+        }
+    }
+}
---
+++ Runtime/Service/EventService.cs
@@ -0,0 +1,124 @@
+﻿using System.Collections;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GameWorkstore.Patterns
+{
+    public class EventService : IService
+    {
+        private EventServiceMonobehaviour _behaviour;
+
+        public Signal Update { get; } = new Signal();
+        public Signal LateUpdate { get; } = new Signal();
+        public Signal ApplicationQuit { get; } = new Signal();
+        public Signal FixedUpdate { get; } = new Signal();
+        public Signal<bool> ApplicationFocus { get; } = new Signal<bool>();
+        public Queue<Action> ActionsPerFrame { get; } = new Queue<Action>();
+
+        public bool IsExitApplicationStarted = false;
+
+        public override void Preprocess()
+        {
+            if (IsExitApplicationStarted) return;
+            _behaviour = new GameObject("EventService").AddComponent<EventServiceMonobehaviour>();
+            _behaviour.gameObject.hideFlags = HideFlags.HideAndDontSave;
+            _behaviour.EventService = this;
+            UnityEngine.Object.DontDestroyOnLoad(_behaviour.gameObject);
+        }
+
+        public override void Postprocess()
+        {
+        }
+
+        public Coroutine StartCoroutine(IEnumerator coroutine)
+        {
+            return _behaviour.StartCoroutine(coroutine);
+        }
+
+        public Coroutine QueueDelayedAction(Action action, float time)
+        {
+            return _behaviour.StartCoroutine(DelayedAction(action, time));
+        }
+
+        private IEnumerator DelayedAction(Action action, float time)
+        {
+            yield return new WaitForSecondsRealtime(time);
+            action?.Invoke();
+        }
+
+        public void QueueAction(Action action)
+        {
+            ActionsPerFrame.Enqueue(action);
+        }
+
+        public void StopCoroutine(Coroutine coroutine)
+        {
+            _behaviour.StopCoroutine(coroutine);
+        }
+
+        internal void ExecuteUpdate()
+        {
+            Update.Invoke();
+            if (ActionsPerFrame.Count <= 0) return;
+            var action = ActionsPerFrame.Dequeue();
+            action?.Invoke();
+        }
+
+        internal void ExecuteLateUpdate()
+        {
+            LateUpdate.Invoke();
+        }
+
+        internal void ExecuteFixedUpdate()
+        {
+            FixedUpdate.Invoke();
+        }
+
+        internal void ExecuteApplicationQuit()
+        {
+            ApplicationQuit.Invoke();
+            IsExitApplicationStarted = true;
+        }
+
+        internal void ExecuteApplicationFocus(bool focus)
+        {
+            ApplicationFocus.Invoke(focus);
+        }
+	}
+
+    public class EventServiceMonobehaviour : MonoBehaviour
+    {
+        internal EventService EventService;
+
+        public void Update()
+        {
+            EventService.ExecuteUpdate();
+        }
+
+        public void LateUpdate()
+        {
+            EventService.ExecuteLateUpdate();
+        }
+
+        public void OnApplicationFocus(bool focus)
+        {
+            EventService.ExecuteApplicationFocus(focus);
+        }
+
+        public void OnApplicationQuit()
+        {
+            EventService.ExecuteApplicationQuit();
+        }
+
+		public void FixedUpdate()
+		{
+            EventService.ExecuteFixedUpdate();
+		}
+
+		public void OnDestroy()
+        {
+            ServiceProvider.Shutdown();
+        }
+    }
+}
