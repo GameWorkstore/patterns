2f152538b8f6bcbfbff71cc4edbe0ec976e6629a
HanzaRu <woodimbr@gmail.com>
2023-01-23 09:44:36 +0100

    Update SignalTest.cs

--- Assets/Tests/EditMode/SignalTest.cs
+++ Assets/Tests/EditMode/SignalTest.cs
@@ -20,4 +20,35 @@ public class SignalTest
         substituible.Received(1).Register(Arg.Any<Action>());
         substituible.Received(1).Unregister(Arg.Any<Action>());
     }
+
+    [Test]
+    public void RegisteredFunctionIsCalled()
+    {
+        var signal = new Signal<int>();
+        var hasCalled = false;
+        void SomeFunction(int i)
+        {
+            hasCalled = true;
+        }
+
+        signal.Register(SomeFunction);
+        signal.Invoke(1);
+        Assert.True(hasCalled);
+    }
+
+    [Test]
+    public void UnregisteredFunctionIsNotCalled()
+    {
+        var signal = new Signal<int>();
+        var hasCalled = false;
+        void SomeFunction(int i)
+        {
+            hasCalled = true;
+        }
+
+        signal.Register(SomeFunction);
+        signal.Unregister(SomeFunction);
+        signal.Invoke(1);
+        Assert.False(hasCalled);
+    }
 }
