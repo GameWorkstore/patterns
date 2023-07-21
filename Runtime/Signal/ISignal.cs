b1996711ff3c3dae7ebcece6984aec1b7c310d4e
HanzaRu <woodimbr@gmail.com>
2023-07-21 23:59:22 +0200

    Minor fix for PlatformInputUpdaterService

---
+++ Runtime/Signal/ISignal.cs
@@ -0,0 +1,36 @@
+using System;
+
+namespace GameWorkstore.Patterns
+{
+    public interface ISignal
+    {
+        void Empty();
+        void Invoke();
+        void Register(Action e);
+        void Unregister(Action e);
+    }
+
+    public interface ISignal<T>
+    {
+        void Empty();
+        void Invoke(T evt);
+        void Register(Action<T> e);
+        void Unregister(Action<T> e);
+    }
+
+    public interface ISignal<T, U>
+    {
+        void Empty();
+        void Invoke(T evt, U evu);
+        void Register(Action<T,U> e);
+        void Unregister(Action<T,U> e);
+    }
+
+    public interface ISignal<T, U, V>
+    {
+        void Empty();
+        void Invoke(T evt, U evu, V evv);
+        void Register(Action<T, U, V> e);
+        void Unregister(Action<T, U, V> e);
+    }
+}
